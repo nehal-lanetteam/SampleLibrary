8432f2fa35418d5b66beca5e2ec4993a10360db1
Nehal Ptael <nehal.lanetteam@gmail.com>
2015-09-10 17:11:08 +0530

    initial commit

---
+++ src/main/java/sdk/tokinu/com/tokinusdk/MainActivity.java
@@ -0,0 +1,22 @@
+package sdk.tokinu.com.tokinusdk;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public static void AuthenticatMe()
+    {
+
+    }
+
+}
